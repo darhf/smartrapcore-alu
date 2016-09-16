b126ebec40dda65b75f430c30881afcabf4b4c10
darhf <bfleire@gmail.com>
2016-09-16 18:28:19 +0200

    changed PID from autotune

--- firmware/Marlin/Configuration.h
+++ firmware/Marlin/Configuration.h
@@ -196,10 +196,17 @@
   #define PID_dT ((OVERSAMPLENR * 10.0)/(F_CPU / 64.0 / 256.0)) //sampling period of the temperature routine
 
 // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
-// Ultimaker
-    #define  DEFAULT_Kp 22.2
-    #define  DEFAULT_Ki 1.08
-    #define  DEFAULT_Kd 114
+
+// darhfcore
+    #define  DEFAULT_Kp 19.97
+    #define  DEFAULT_Ki 1.35
+    #define  DEFAULT_Kd 74.01
+
+
+// Ultimaker (previous)
+  //  #define  DEFAULT_Kp 22.2
+  //  #define  DEFAULT_Ki 1.08
+  //  #define  DEFAULT_Kd 114
 
 // MakerGear
 //    #define  DEFAULT_Kp 7.0
